775532d0a24280e01558abc80c72a4abbd755060
Jamie Dwyer <jamie.dwyer@shopify.com>
2018-02-09 00:54:15 -0500

    Fix whitespace (again)

--- src/public/helpers.php
+++ src/public/helpers.php
@@ -16,21 +16,21 @@ function getAccessToken($shop, $apiKey, $secret, $code) {
   	'client_id' => $apiKey,
   	'client_secret' => $secret,
   	'code' => $code
-	);
+  );
 
-	// Build access token URL
-	$access_token_url = "https://{$shop}/admin/oauth/access_token";
+  // Build access token URL
+  $access_token_url = "https://{$shop}/admin/oauth/access_token";
 
-	// Configure curl client and execute request
-	$curl = curl_init();
+  // Configure curl client and execute request
+  $curl = curl_init();
   $curlOptions = array(
     CURLOPT_RETURNTRANSFER => TRUE,
     CURLOPT_URL => $access_token_url,
     CURLOPT_POSTFIELDS => http_build_query($query)
   );
-	curl_setopt_array($curl, $curlOptions);
-	$jsonResponse = json_decode(curl_exec($curl), TRUE);
-	curl_close($curl);
+  curl_setopt_array($curl, $curlOptions);
+  $jsonResponse = json_decode(curl_exec($curl), TRUE);
+  curl_close($curl);
 
   return $jsonResponse['access_token'];
 }
